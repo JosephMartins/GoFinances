52081cdc1fa3ca8f1ad502187fc4a10314c9372a
JosephMartins <neto-neon@live.com>
2020-05-01 17:16:38 -0300

    Finished challange

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
-
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
@@ -23,19 +21,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
+    console.log(data);
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.goBack();
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const propsFile = files.map(file => ({
+      file,
+
+      name: file.name,
+      readableSize: String(file.size),
+    }));
+
+    setUploadedFiles(propsFile);
   }
 
   return (
